9901f6f50c259889af186065b589a07ef34df04f
VineethThumma <vineethreddy.2704@gmail.com>
2017-09-02 01:14:09 -0400

    Restaurant Simulation

---
+++ src/Order.java
@@ -0,0 +1,17 @@
+
+public class Order {
+	int burgers;
+	int fries;
+	int coke;
+	int sundae;
+	int diner_id;
+	
+	public Order(int burgers, int fries, int coke, int sundae, int diner_id) {
+		this.burgers = burgers;
+		this.fries = fries;
+		this.coke = coke;
+		this.sundae = sundae;
+		this.diner_id = diner_id;
+	}
+	
+}
